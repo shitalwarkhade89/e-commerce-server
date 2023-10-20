57b0410155b1dd3b92ffcb67e96a05cda88e3573
shitalwarkhade89 <shitalwarkhade89@gmail.com>
2023-10-20 20:33:52 +0530

    mongodb connected successfully

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express from "express";
 import mongoose, { model, Schema } from 'mongoose';
-
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 const app = express();
 app.use(express.json());
 
 const PORT = 5000;
+
+// mongodb connection
+
+const connectMongoDb = async () => {
+    const conn = await mongoose.connect(process.env.MONGODB_URI)
+    if (conn) {
+        console.log('Mongodb connected succesfully.');
+    }
+};
+connectMongoDb();
 
 
 // get products
